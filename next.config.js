3594e76f54b9a0bf203ebb708dc4828d672072bc
krzysiekkedziora-star <krzysiek.kedziora@gmail.com>
2026-06-01 00:51:29 +0200

    Enable static export for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -4,10 +4,13 @@ const createNextIntlPlugin = require('next-intl/plugin');
 const withNextIntl = createNextIntlPlugin('./i18n.ts');
 
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   images: {
     unoptimized: true,
   },
+  basePath: '',
+  trailingSlash: true,
 };
 
 module.exports = withNextIntl(nextConfig);
