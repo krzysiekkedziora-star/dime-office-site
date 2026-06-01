cfa1f64437c580930860401bfaab321226437cfd
krzysiekkedziora-star <krzysiek.kedziora@gmail.com>
2026-06-01 14:05:00 +0200

    Update i18n.ts

--- i18n.ts
+++ i18n.ts
@@ -1,5 +1,8 @@
 import {getRequestConfig} from 'next-intl/server';
 
+export const locales = ['en', 'pl', 'uk'] as const;
+export const defaultLocale = 'pl' as const;
+
 export default getRequestConfig(async ({locale}) => ({
   messages: (await import(`./messages/${locale}.json`)).default
 }));
